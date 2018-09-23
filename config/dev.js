798257bcb73a18c48c9aa92f32c513a6b8212468
Calvin Colton <calvincolton@gmail.com>
2018-09-23 13:38:20 -0700

    upload routes

--- config/dev.js
+++ config/dev.js
@@ -4,5 +4,7 @@ module.exports = {
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
   mongoURI: 'mongodb://cal-test:password22@ds231242.mlab.com:31242/blogster-dev',
   cookieKey: '123123123',
-  redisUrl: 'redis://127.0.0.1:6379'
+  redisUrl: 'redis://127.0.0.1:6379',
+  accessKeyId: 'AKIAJ7VQHFUAHDALUIRA',
+  secretAccessKey: '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'
 };
